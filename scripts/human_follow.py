859e323d38d1f4af9a47f87eed36c396b1770deb
ShunjiHashimoto <h.shunji1206@gmail.com>
2022-02-03 09:46:49 +0900

    更新式で相対速度に対する相対速度みたいな計算になっていたのを修正した

--- scripts/human_follow.py
+++ scripts/human_follow.py
@@ -22,10 +22,10 @@ class KalmanFilter:
         self.robot_pose = np.array([0.0, 0.0, math.pi/2]).T   
         self.r = 0.2            
         self.color = "black"   
-        self.robot_vel = 1.0                        
-        self.robot_omega = 0.00001                                
+        self.robot_vel = 1.0                     
+        self.robot_omega = 0.001                              
         self.time_interval = 0.1
-        self.human_pose_from_robot = np.array([1.0, 0.0, 0.0, 1.0, 0.0]).T # 人の座標(x, y, z, x', y')
+        self.human_pose_from_robot = np.array([1.0, 0.0, 0.0, 0.1, 1.0]).T # 人の座標(x, y, z, x', y'), 速度はworld座標系で見たときの速度
         self.human_pose_from_world = np.array([0.0, 1.0]).T # 人座標(x, y, z, x', y')
 
         self.distance_range = (0.0, 10.0)
@@ -58,7 +58,6 @@ class KalmanFilter:
         delta_l = 2*v*math.sin(delta_theta/2)/w
         delta_x = delta_l*math.cos(delta_theta/2)
         delta_y = delta_l*math.sin(delta_theta/2)
-        print(vx, delta_x)
         return np.array([ (xt_1 + t*vx - delta_x)*math.cos(delta_theta) + (yt_1 + t*vy - delta_y)*math.sin(delta_theta), \
                           -(xt_1 + t*vx - delta_x)*math.sin(delta_theta) + (yt_1 + t*vy - delta_y)*math.cos(delta_theta), \
                           zt_1, \
@@ -66,11 +65,11 @@ class KalmanFilter:
                           -vx*math.sin(delta_theta) + vy*math.cos(delta_theta) ])
     
     @classmethod
-    def cals_l_and_phai(cls, human_pose, robot_pose): # human_pose(x, y, z, x', y')のxはworldでは高さ方向y, yは横方向x
-        x = human_pose[1] - robot_pose[0] # world
-        y = human_pose[0] - robot_pose[1] # world
+    def cals_l_and_phai(cls, human_pose, robot_pose):
+        x = human_pose[1]
+        y = human_pose[0]
         diff = np.array([x,y])
-        phi = math.atan2(diff[0], diff[1]) # robot
+        phi = math.atan2(diff[0], diff[1])
         while phi >= np.pi: phi -= 2*np.pi
         while phi < -np.pi: phi  += 2*np.pi
         return np.hypot(*diff), phi # hypot: 距離を返す
@@ -83,10 +82,10 @@ class KalmanFilter:
         elems += ax1.plot([x,xn], [y,yn], color=self.color) # ロボットの向きを示す線分の描画
         elems.append(ax1.add_patch(c_robot))                # 上のpatches.Circleでロボットの胴体を示す円を作ってサブプロットへ登録
         l, phi = self.cals_l_and_phai(self.human_pose_from_robot, self.robot_pose)
-        # self.human_pose_from_world = np.array([ self.robot_pose[0] + l*math.cos( -phi+ self.robot_pose[2]), \
-        #      self.robot_pose[1] + l*math.sin( -phi + self.robot_pose[2]) ])
-        self.human_pose_from_world = np.array([ self.robot_pose[0] + self.human_pose_from_robot[1],  \
-             self.robot_pose[1] + self.human_pose_from_robot[0] ])
+        self.human_pose_from_world = np.array([ self.robot_pose[0] + l*math.cos( -phi+ self.robot_pose[2]), \
+             self.robot_pose[1] + l*math.sin( -phi + self.robot_pose[2]) ])
+        # self.human_pose_from_world = np.array([ self.robot_pose[0] + self.human_pose_from_robot[1],  \
+        #      self.robot_pose[1] + self.human_pose_from_robot[0] ])
         elems += ax1.plot(self.human_pose_from_world[0], self.human_pose_from_world[1], "red", marker = '*', markersize = 8)
         if self.robot_visible_range(l, phi):
             zx = self.human_pose_from_world[0]
@@ -98,6 +97,7 @@ class KalmanFilter:
         self.robot_pose = self.robot_state_transition(self.robot_vel, self.robot_omega, self.time_interval, self.robot_pose)  # ロボットの姿勢を更新
         self.human_pose_from_robot = self.calc_human_pose_from_robot(self.human_pose_from_robot[0], self.human_pose_from_robot[1], \
             self.human_pose_from_robot[2], self.human_pose_from_robot[3], self.human_pose_from_robot[4], self.robot_omega, self.robot_vel, self.time_interval)
+        self.human_pose_from_robot[3] += self.robot_vel
 
     def map_draw(self):
         fig = plt.figure(figsize=(10,10)) 
